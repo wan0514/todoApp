f108170548e68fdd088718c2b6994665daf71c4d
wan <conut22@naver.com>
2024-11-12 18:57:00 +0900

    초기 세팅 완료

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,32 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        slate900: '#0F172A',
+        slate800: '#1E293B',
+        slate500: '#64748B',
+        slate400: '#94A3B8',
+        slate300: '#CBD5E1',
+        slate200: '#E2E8F0',
+        slate100: '#F1F5F9',
+
+        violet600: '#7C3AED',
+        violet100: '#EDE9FE',
+        rose500: '#F43F5E',
+        rose300: '#BEF264',
+        amber800: '#92400E',
+      },
+      fontFamily: {
+        NanumSquareB: 'var(--font-NanumSquareB)',
+        NanumSquareR: 'var(--font-NanumSquareR)',
       },
     },
   },
